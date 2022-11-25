d19bbd509f1c1d7be13a5155d3f065afe8ee2d96
hrushitha <hrushithachinthakayalc@gmail.com>
2022-11-25 13:01:36 +0530

    first commit

---
+++ src/main/java/com/webapp/SampleMavenExam/App.java
@@ -0,0 +1,13 @@
+package com.webapp.SampleMavenExam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam ,The created project is SampleMavenExam" );
+    }
+}
